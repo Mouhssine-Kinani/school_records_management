7dd59a98000fdc1f64a26c727c5c57557f98a13c
Mouhssine Kinani <kinanimouhssine@gmail.com>
2025-12-28 21:58:29 +0100

    adding the matiere libelle to the cards

--- src/Repository/ClasseRepository.php
+++ src/Repository/ClasseRepository.php
@@ -32,14 +32,16 @@ class ClasseRepository extends ServiceEntityRepository
                 c.annee_scolaire,
                 COUNT(DISTINCT i.id) as student_count,
                 u.nom as teacher_nom,
-                u.prenom as teacher_prenom
+                u.prenom as teacher_prenom,
+                m.libelle as matiere_libelle
             FROM classe c
             LEFT JOIN enseignant_matiere_classe emc ON c.id = emc.classe_id
             LEFT JOIN inscription i ON c.id = i.classe_id 
                 AND i.annee_scolaire = c.annee_scolaire 
                 AND i.statut = :statut
             LEFT JOIN utilisateur u ON emc.enseignant_id = u.id AND u.role = :role
-            GROUP BY c.id, c.nom, c.niveau, c.annee_scolaire, u.nom, u.prenom
+            LEFT JOIN matiere m ON emc.matiere_id = m.id
+            GROUP BY c.id, c.nom, c.niveau, c.annee_scolaire, u.nom, u.prenom, m.libelle
             ORDER BY c.niveau ASC, c.nom ASC
         ';
         
