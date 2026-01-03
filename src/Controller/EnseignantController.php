13a42ce9f8433234547cc5b341d25b309454cb12
soukaina <soukainalakhmaiss743@gmail.com>
2026-01-04 00:48:51 +0100

    Ajout dashboard enseignant et fonctionnalités

--- src/Controller/EnseignantController.php
+++ src/Controller/EnseignantController.php
@@ -6,16 +6,38 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use App\Repository\EnseignantMatiereClasseRepository;
+use App\Repository\NoteRepository;
+use App\Repository\InscriptionRepository;
 
 #[Route('/enseignant')]
 #[IsGranted('ROLE_ENSEIGNANT')]
 class EnseignantController extends AbstractController
 {
     #[Route('/dashboard', name: 'enseignant_dashboard')]
-    public function dashboard(): Response
-    {
+    public function dashboard(
+        EnseignantMatiereClasseRepository $emcRepo,
+        NoteRepository $noteRepo,
+        InscriptionRepository $inscriptionRepo
+    ): Response {
+        $enseignant = $this->getUser();
+
+        // Classes + matières de l’enseignant
+        $affectations = $emcRepo->findBy([
+            'enseignant' => $enseignant
+        ]);
+
+        // Dernières notes saisies par l’enseignant
+        $dernieresNotes = $noteRepo->findBy(
+            ['enseignant' => $enseignant],
+            ['dateNote' => 'DESC'],
+            5
+        );
+
         return $this->render('enseignant/dashboard.html.twig', [
-            'user' => $this->getUser(),
+            'user' => $enseignant,
+            'affectations' => $affectations,
+            'dernieresNotes' => $dernieresNotes,
         ]);
     }
 }
